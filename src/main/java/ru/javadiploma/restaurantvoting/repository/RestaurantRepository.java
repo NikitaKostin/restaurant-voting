5b658d6abf83db4d79a3a12b2248a28d00b85d6c
Nikita Kostin <n.kostin11@mail.ru>
2023-05-14 15:21:19 +0300

    Delete useless findById

--- src/main/java/ru/javadiploma/restaurantvoting/repository/RestaurantRepository.java
+++ src/main/java/ru/javadiploma/restaurantvoting/repository/RestaurantRepository.java
@@ -3,15 +3,12 @@ package ru.javadiploma.restaurantvoting.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import ru.javadiploma.restaurantvoting.model.Restaurant;
 
 import java.util.Optional;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
-    Optional<Restaurant> findById(int id);
-
     @Query("SELECT r FROM Restaurant r JOIN FETCH r.menus m WHERE r.id= ?1 AND m.createDate = current_date ")
     Optional<Restaurant> getWithRestaurant(int id);
 }
