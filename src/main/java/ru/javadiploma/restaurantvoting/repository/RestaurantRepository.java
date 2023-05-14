6df9dc13ace3b26c810fc7817fcd4e5fa68801a6
Nikita Kostin <n.kostin11@mail.ru>
2023-05-14 20:36:49 +0300

    Add dish to getWithMenu

--- src/main/java/ru/javadiploma/restaurantvoting/repository/RestaurantRepository.java
+++ src/main/java/ru/javadiploma/restaurantvoting/repository/RestaurantRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
-    @Query("SELECT r FROM Restaurant r JOIN FETCH r.menus m WHERE r.id= ?1 AND m.createDate = current_date ")
+    @Query("SELECT r FROM Restaurant r JOIN FETCH r.menus m JOIN FETCH m.dish WHERE r.id= ?1 AND m.createDate = current_date")
     Optional<Restaurant> getWithRestaurant(int id);
 }
