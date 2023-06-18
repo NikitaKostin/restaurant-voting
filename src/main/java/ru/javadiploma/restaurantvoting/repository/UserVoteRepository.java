a5966d11d41ec66219d855a9fd15af711a47895c
Nikita Kostin <n.kostin11@mail.ru>
2023-06-18 13:05:48 +0300

    Update vote

--- src/main/java/ru/javadiploma/restaurantvoting/repository/UserVoteRepository.java
+++ src/main/java/ru/javadiploma/restaurantvoting/repository/UserVoteRepository.java
@@ -1,7 +1,12 @@
 package ru.javadiploma.restaurantvoting.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.javadiploma.restaurantvoting.model.User;
 import ru.javadiploma.restaurantvoting.model.UserVote;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface UserVoteRepository extends JpaRepository<UserVote, Integer> {
+    Optional<UserVote> getByUserAndVoteDate(User user, LocalDate voteDate);
 }
--- src/main/java/ru/javadiploma/restaurantvoting/service/UserVoteService.java
+++ src/main/java/ru/javadiploma/restaurantvoting/service/UserVoteService.java
@@ -2,14 +2,17 @@ package ru.javadiploma.restaurantvoting.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.javadiploma.restaurantvoting.error.IllegalRequestDataException;
 import ru.javadiploma.restaurantvoting.model.UserVote;
 import ru.javadiploma.restaurantvoting.repository.RestaurantRepository;
 import ru.javadiploma.restaurantvoting.repository.UserRepository;
 import ru.javadiploma.restaurantvoting.repository.UserVoteRepository;
 import ru.javadiploma.restaurantvoting.to.UserVoteTo;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Optional;
 
 @Service
 public class UserVoteService {
@@ -21,21 +24,25 @@ public class UserVoteService {
     protected RestaurantRepository restaurantRepository;
 
     public UserVote vote(UserVoteTo userVoteTo, int userId) {
-        return userVoteRepository.save(new UserVote(
-                null,
-                userRepository.getById(userId),
-                restaurantRepository.getById(userVoteTo.getRestaurantId())
-        ));
-    }
+        Optional<UserVote> userVote = userVoteRepository.getByUserAndVoteDate(userRepository.getById(userId), LocalDate.now());
 
-    public UserVote changeVote(UserVoteTo userVoteTo, int userId) {
-        if (LocalDateTime.now().toLocalTime().isBefore(LocalTime.of(11, 0, 0))) {
+        if (userVote.isPresent()) {
+            if (LocalDateTime.now().toLocalTime().isBefore(LocalTime.of(11, 0, 0))) {
+                userVote
+                        .ifPresent(it -> {
+                            it.setRestaurant(restaurantRepository.getById(userVoteTo.getRestaurantId()));
+                            userVoteRepository.save(it);
+                        });
+            } else {
+                throw new IllegalRequestDataException("To late to change vote");
+            }
+        } else {
             return userVoteRepository.save(new UserVote(
                     null,
                     userRepository.getById(userId),
                     restaurantRepository.getById(userVoteTo.getRestaurantId())
             ));
         }
-        return null;
+        return userVote.get();
     }
 }
