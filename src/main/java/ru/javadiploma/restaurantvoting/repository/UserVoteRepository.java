b58b7e871d61943409ac586159e789803885d240
Nikita Kostin <n.kostin11@mail.ru>
2023-06-18 14:51:10 +0300

    Update vote controller

--- src/main/java/ru/javadiploma/restaurantvoting/repository/UserVoteRepository.java
+++ src/main/java/ru/javadiploma/restaurantvoting/repository/UserVoteRepository.java
@@ -5,8 +5,11 @@ import ru.javadiploma.restaurantvoting.model.User;
 import ru.javadiploma.restaurantvoting.model.UserVote;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserVoteRepository extends JpaRepository<UserVote, Integer> {
     Optional<UserVote> getByUserAndVoteDate(User user, LocalDate voteDate);
+
+    List<UserVote> getAllByUser(User user);
 }
--- src/main/java/ru/javadiploma/restaurantvoting/web/vote/UserVoteController.java
+++ src/main/java/ru/javadiploma/restaurantvoting/web/vote/UserVoteController.java
@@ -5,12 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import ru.javadiploma.restaurantvoting.error.ResourceNotFoundException;
 import ru.javadiploma.restaurantvoting.model.UserVote;
+import ru.javadiploma.restaurantvoting.repository.UserRepository;
+import ru.javadiploma.restaurantvoting.repository.UserVoteRepository;
 import ru.javadiploma.restaurantvoting.service.UserVoteService;
 import ru.javadiploma.restaurantvoting.to.UserVoteTo;
 import ru.javadiploma.restaurantvoting.web.AuthUser;
 
 import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = UserVoteController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -20,9 +25,27 @@ public class UserVoteController {
     @Autowired
     private UserVoteService userVoteService;
 
+    @Autowired
+    private UserVoteRepository userVoteRepository;
+
+    @Autowired
+    protected UserRepository userRepository;
+
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public UserVote vote(@Valid @RequestBody UserVoteTo userVoteTo, @AuthenticationPrincipal AuthUser authUser) {
         return userVoteService.vote(userVoteTo, authUser.id());
+    }
+
+    @GetMapping("/today")
+    public UserVote get(@AuthenticationPrincipal AuthUser authUser) {
+        return userVoteRepository.getByUserAndVoteDate(userRepository.getById(authUser.id()), LocalDate.now()).orElseThrow(
+                () -> new ResourceNotFoundException("You haven't voted today")
+        );
+    }
+
+    @GetMapping("/all")
+    public List<UserVote> getAll(@AuthenticationPrincipal AuthUser authUser) {
+        return userVoteRepository.getAllByUser(userRepository.getById(authUser.id()));
     }
 }
