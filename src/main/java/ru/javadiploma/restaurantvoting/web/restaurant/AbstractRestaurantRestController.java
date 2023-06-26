0ec450660d4944168727e84875976d623e9892fe
Nikita Kostin <n.kostin11@mail.ru>
2023-06-26 10:49:35 +0300

    Fix error text

--- src/main/java/ru/javadiploma/restaurantvoting/web/restaurant/AbstractRestaurantRestController.java
+++ src/main/java/ru/javadiploma/restaurantvoting/web/restaurant/AbstractRestaurantRestController.java
@@ -3,10 +3,8 @@ package ru.javadiploma.restaurantvoting.web.restaurant;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.javadiploma.restaurantvoting.error.ResourceNotFoundException;
-import ru.javadiploma.restaurantvoting.model.MenuItem;
 import ru.javadiploma.restaurantvoting.model.Restaurant;
 import ru.javadiploma.restaurantvoting.repository.RestaurantRepository;
-import ru.javadiploma.restaurantvoting.service.MenuItemService;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -26,7 +24,7 @@ public abstract class AbstractRestaurantRestController {
     public Restaurant getRestaurantMenuItemsWithDish(int id) {
         log.info("get restaurant menu items {} with dish", id);
         return restaurantRepository.getRestaurantMenuItemsWithDishByDate(id, LocalDate.now()).orElseThrow(
-                () -> new ResourceNotFoundException("Restaurant with id " + id + " not found")
+                () -> new ResourceNotFoundException("Not found today menu item for restaurant with id " + id)
         );
     }
 
